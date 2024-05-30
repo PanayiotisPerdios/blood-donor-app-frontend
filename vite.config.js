f9c3baa05783326e6effda786670488a3d227a67
Panayiotis Perdios <panos.perdios@gmail.com>
2024-05-30 23:01:05 +0300

    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import Components from 'unplugin-vue-components/vite';
-import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig(({ mode }) =>{
 
   return {
     server: {
@@ -18,19 +15,14 @@ export default defineConfig(({ mode }) => {
               // secure: false, 
             }
           }
-  },
+  },        
   plugins: [
     vue(),
-    Components({
-      resolvers: [
-          BootstrapVueNextResolver()
-      ],
-    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-      },
+    }
   }
 };
 });
